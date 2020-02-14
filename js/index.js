ddead227b5d485628a33812ca5591078f8d2cd42
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2020-02-13 22:12:06 -0800

    propogation and preventDefault completed

--- js/index.js
+++ js/index.js
@@ -13,15 +13,10 @@ navBars.forEach(nav => {
 });
 
 // key down
-// const keyboardKey = document.querySelectorAll('p');
-// console.log(keyboardKey)
-// keyboardKey.addEventListener('keydown', (event) => {
-//     if (event.keyCode == 1) {
-//         event.style.background = 'red';
-//     } else {
-//         event.style.background = '';
-//     }
-// })
+const keyboardKey = document.querySelectorAll('p');
+keyboardKey[1].addEventListener('keydown', (event) => {
+    event.target.style.color = 'red';
+});
 
 // focus and blur
 const focusNav = document.querySelectorAll('.nav a');
@@ -34,3 +29,48 @@ focusNav.forEach(nav => {
         e.target.style.color = 'blue';
     })
 })
+
+const focusH2 = document.querySelectorAll('.btn');
+console.log(focusH2)
+focusH2.forEach(t => {
+    t.addEventListener('focus', (f) => {
+    f.target.style.color = 'red';
+    })
+    t.addEventListener('blur', (b) => {
+        b.target.style.color = 'blue';
+    })
+})
+
+
+// stop propogation
+const bodyColor = document.querySelector('body');
+console.log(bodyColor)
+
+bodyColor.addEventListener('click', (event) => {
+    bodyColor.style.backgroundColor = 'yellow';
+    event.stopPropagation();
+})
+
+const titleColor = document.querySelectorAll('h2');
+console.log(titleColor)
+titleColor.forEach(color => {
+    color.addEventListener('click', (event) => {
+        color.style.backgroundColor = 'blue';
+        event.stopPropagation();
+})
+})
+
+
+// dblclick
+// prevent default action
+const stopRefresh = document.querySelectorAll('.nav-link');
+console.log(stopRefresh);
+stopRefresh.forEach(navLink => {
+    navLink.addEventListener('dblclick', (event) => {
+    event.preventDefault();
+    console.log('stop refresh');
+    })
+})
+
+
+
