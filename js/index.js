7eee21e220ab1414f10efae75d6970ae069f5887
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2020-02-13 19:46:56 -0800

    project updated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// Adding event listeners
+
+// mouse over
+const navBars = document.querySelectorAll('.nav a');
+navBars.forEach(nav => {
+    nav.addEventListener('mouseover', () => {
+        if (nav.style.color === 'orange') {
+            nav.style.color = 'black';
+        } else {
+            nav.style.color = 'orange';
+        }
+    })
+});
+
+// key down
+const keyboardKey = document.querySelectorAll('p');
+keyboardKey.addEventListener('keydown', (event) => {
+    if (event.keyCode == 1) {
+        event.style.background = 'red';
+    } else {
+        event.style.background = '';
+    }
+})
